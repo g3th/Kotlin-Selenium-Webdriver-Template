72f1a35ab3af3ab24a67a81833270d46120e3ee9
g3th <theinkandpen@yahoo.co.uk>
2022-10-04 06:34:43 +0100

    modified:   .gitignore 	modified:   package_scraper.kt 	libs/jsoup-1.15.3.jar

--- package_scraper.kt
+++ package_scraper.kt
@@ -1,3 +1,7 @@
+import org.jsoup.*
+import org.openqa.selenium.JavascriptExecutor
+import org.openqa.selenium.By
+import org.openqa.selenium.WebElement
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.remote.RemoteWebDriver
 import org.openqa.selenium.devtools.HasDevTools
@@ -6,9 +10,27 @@ import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.chromium.ChromiumDriver
 
 fun main(){
+
+	System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver")
+	print("\nEnter Query: ")
+	var userQuery = readLine()
+	
+	var scrollIncrement:Int = 2000
+	var counter:Int = 0
+	var linksList:List<String> = emptyList()
 	val browserOptions = ChromeOptions()
-	browserOptions.addArguments("""general.useragent.override","Mozilla/5.0 (iPhone; CPU iPhone OS 12_1 like Mac OS X) AppleWebKit/605.1.15 ","(KHTML, like Gecko) FxiOS/18.1 Mobile/16B92 Safari/605.1.15""")
-	System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver")
 	val browser: WebDriver = ChromeDriver(browserOptions)
-	browser.get("https://www.google.co.uk/search?q=bananas&tbm=isch&source=hp&biw=1380&bih=702&ei=AG87Y7-2D8mGhbIPtLSWgAQ&iflsig=AJiK0e8AAAAAYzt9EFdV6J06nCQ1wxANNR_CsaAQmE5J&ved=0ahUKEwi_tvaTmsX6AhVJQ0EAHTSaBUAQ4dUDCAc&uact=5&oq=bananas&gs_lcp=CgNpbWcQAzIICAAQgAQQsQMyCAgAEIAEELEDMggIABCABBCxAzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgARQgQZY-BBg5xFoAXAAeACAAWeIAd0EkgEDNi4xmAEAoAEBqgELZ3dzLXdpei1pbWewAQA&sclient=img")
+	
+	browserOptions.addArguments("""general.useragent.override","Mozilla/5.0 (iPhone; CPU iPhone OS 12_1 like Mac OS X) AppleWebKit/605.1.15 ","(KHTML, like Gecko) FxiOS/18.1 Mobile/16B92 Safari/605.1.15""")
+	
+	browser.get("https://www.pexels.com/search/${userQuery}")
+	while (counter !=5){	
+			(browser as JavascriptExecutor).executeScript("window.scrollBy(0,${scrollIncrement})")
+			var rawPage = browser.findElement(By.xpath("//*[@href]"))
+			print(rawPage)
+			linksList.plus(rawPage.toString())
+			scrollIncrement + 2000
+			++counter
+		}
+	println(linksList)
 }
