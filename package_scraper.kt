3334bab7c47e0354d5d59216675dac34f291bce9
g3th <theinkandpen@yahoo.co.uk>
2022-10-07 02:48:20 +0100

    works, but scrapgin/sorting is wrong for lazyloading (repeats images) 	modified:   package_scraper.kt 	test.kt

--- package_scraper.kt
+++ package_scraper.kt
@@ -11,39 +11,60 @@ import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
 
+fun clearScreen(){
+	println("\u001b[H\u001b[2J")
+	}
 
 fun main(){
+
 	val directory = Paths.get("").toAbsolutePath().toString()
 	File(directory + "/downloads/").mkdir()
-	System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver")
 	print("\nEnter Query: ")
 	var findTags: List<WebElement> = emptyList()
-	var userQuery = readLine()	
+	var imageQuery = readLine()
+	print("\nEnter number of Pages to scrape: ")
+	var numberOfPages = readLine() 
 	var scrollIncrement:Int = 2000
 	var counter:Int = 0
 	var linksList:List<String> = emptyList()
 	val browserOptions = ChromeOptions()
+	browserOptions.addArguments("--headless")
+	System.setProperty("http.agent","Chrome")
 	browserOptions.addArguments("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.101 Safari/537.36")
-	val browser: WebDriver = ChromeDriver(browserOptions)	
-	browser.get("https://www.pexels.com/search/${userQuery}")
+
+	var browser: WebDriver = ChromeDriver(browserOptions)	
+	browser.get("https://www.pexels.com/search/${imageQuery}")
 	
-	while (counter !=2){
+	while (counter != numberOfPages!!.toIntOrNull()){
+			clearScreen()
+			println("Fetching Images on Page ${counter.toString()}")	
 			findTags = findTags.plus(browser.findElements(By.xpath("//*[@href]")))
 			Thread.sleep(2000)			
 			scrollIncrement + 2000
 			++counter
 		}
 	for (tag in findTags){
+		clearScreen()
+		println("Scraper returned ${findTags.size} links \nsorting into images (Found ${linksList.size.toString()}")
 		if ("&fm=jpg" in tag.getAttribute("href")){
 			linksList = linksList.plus(tag.getAttribute("href"))
 			}
 		}
-	
+	browser.close()
 	var imageCounter = 0
+	var imageLinks:List<String> = emptyList()
+	var splitImageLink:List<String> = emptyList()
 	for (link in linksList){
-		var url = URL(link)
-		url.openStream().use {
-			Files.copy(it, Paths.get("Image"+imageCounter.toString()))
+		splitImageLink = link.split("?cs=")
+		imageLinks = imageLinks.plus(splitImageLink[0])
+		}
+	for (image in imageLinks){
+		clearScreen()
+		println("Downloading Image ${imageCounter.toString()}")
+		var imageURL = URL(image)
+		imageURL.openStream().use{
+			Files.copy(it, Paths.get("downloads/${imageQuery}_image${imageCounter.toString()}.jpg"))
 			}
+		++ imageCounter
 		}
 }
